64c509891d9dfd509ef7c8b2ad73fbdc5db73bbb
Shreetej Hadge <shreetejhadge1308@gmail.com>
2025-11-16 12:19:27 -0700

    feat: Implement basic page structure with dark mode
    - Set up EJS as the template engine.
    - Add a default dark mode stylesheet.
    - Create an index route and view.
    - Serve static assets from the public directory.

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 require("dotenv").config();
 const express = require("express")
 const path = require("node:path")
+const indexRouter = require('./routes/index')
 
 const app = express()
 
-app.get("/", (req, res) => res.send("Hello, world!"));
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use("/", indexRouter)
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, (error) => {
