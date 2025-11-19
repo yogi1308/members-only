7ec1a77d77609d018a651fbcf889c6a8dbefba32
Shreetej Hadge <shreetejhadge1308@gmail.com>
2025-11-18 23:31:39 -0700

    feat(auth): initialize authentication with Passport
    - Add `passport`, `express-session`, and `bcryptjs` dependencies
    - Configure session and passport middleware in `app.js`
    - Create `config/passport.js` for authentication strategies

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ require("dotenv").config();
 const express = require("express")
 const path = require("node:path")
 const indexRouter = require('./routes/index')
+const session = require("express-session");
+const passport = require("passport");
 
 const app = express()
 
@@ -11,6 +13,9 @@ app.use(express.urlencoded({ extended: true }));
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+
+app.use(session({ secret: process.env.SESSION_SECRET, resave: false, saveUninitialized: false }));
+app.use(passport.session());
 
 app.use("/", indexRouter)
 
